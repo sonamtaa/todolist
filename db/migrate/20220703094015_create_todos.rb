dfe20fc3de8ac52154694a13227fb1501965bf97
sonamtaa <sonam.tashi@selise.ch>
2022-07-04 23:45:58 +0600

    initial rails api

---
+++ db/migrate/20220703094015_create_todos.rb
@@ -0,0 +1,12 @@
+# frozen_string_literal: true
+
+class CreateTodos < ActiveRecord::Migration[7.0]
+  def change
+    create_table :todos do |t|
+      t.string :title
+      t.boolean :completed
+
+      t.timestamps
+    end
+  end
+end
